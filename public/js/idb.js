36a4c0edf473548379cc7a0a623661f434e68e29
Anomalous Alpha <blackcarrera@msn.com>
2021-07-22 19:48:09 -0600

    idb.js file written but still not creating indexedDB, console log shows index.js line 139 saveRecord not defined

--- public/js/idb.js
+++ public/js/idb.js
@@ -4,25 +4,25 @@ let db;
 const request = indexedDB.open('budget_tracker', 1);
 
 // this event will eimit if the database vesion changes (noneexistant to version 1, v1 to v2 etc.)
-request.onupgradeneeded = function(event) {
+request.onupgradeneeded = function (event) {
     // save a reference to the database
     const db = event.target.result;
     //create an obejct store (table) called `new_wallet`, set it to have an auto incrementing primay key of sorts
-    db.createObjectStore('new_wallet', {autoIncrement: true });
+    db.createObjectStore('new_wallet', { autoIncrement: true });
 }
 
 // upon a successful
-request.onsuccess = function(event) {
+request.onsuccess = function (event) {
     // when the db is successfully created with its obect store (from onupgradeneeded event above) or simply established a connection, then save reference to the db in global variable
     db = event.target.result;
 
     // check if ap is online, if yes run uploadWallet() function to send all local db data to api
     if (navigator.onLine) {
-        // uploadWallet();
+        uploadWallet();
     }
 };
 
-request.onerror = function(event) {
+request.onerror = function (event) {
     // log error here
     console.log(event.target.errorCode);
 }
@@ -33,8 +33,51 @@ function saveRecord(record) {
     const transaction = db.transaction(['new_Wallet'], 'readwrite');
 
     // access the object store for `new_Wallet`
-    const walletObjectStore = transaction.createObjectStore('new_wallet');
+    const walletObjectStore = transaction.objectStore('new_wallet');
 
     // add record ot your store with add method
     walletObjectStore.add(record);
 }
+
+function uploadWallet() {
+    // open a transaction on your pending db
+    const transaction = db.transaction(['new_wallet'], 'readwrite');
+
+    // access your pending object store
+    const walletObjectStore = transaction.objectStore('new_wallet');
+
+    // get all records from store and set to a variable
+    const getAll = walletObjectStore.getAll();
+
+    getAll.onsuccess = function () {
+        // if there was data in indexedDb's store, let's send it to the api server
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(response => response.json())
+                .then(serverResponse => {
+                    if (serverResponse.message) {
+                        throw new Error(serverResponse);
+                    }
+
+                    const transaction = db.transaction(['new_wallet'], 'readwrite');
+                    const walletObjectStore = transaction.objectStore('new_wallet');
+                    // clear all items in your store
+                    walletObjectStore.clear();
+                })
+                .catch(err => {
+                    // set reference to redirect back here
+                    console.log(err);
+                });
+        }
+    };
+}
+
+// listen for app coming back online
+window.addEventListener('online', uploadWallet);
