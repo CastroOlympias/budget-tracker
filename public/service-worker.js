4d5684077af9a45a72d703c5f5b00c07070989a2
Anomalous Alpha <blackcarrera@msn.com>
2021-07-23 17:58:21 -0600

    fixed, had server-wroker insted of server-worker and style, instead of styles, now getting a 200 on both files an no error in dev tools

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,8 +5,8 @@ const CACHE_NAME = APP_PREFIX + VERSION;
 const FILES_TO_CACHE = [
   "./index.html",
   "./manifest.json",
-  "./service-wroker.js",
-  "./css/style.css",
+  "./service-worker.js",
+  "./css/styles.css",
   "./icons/icon-72x72.png",
   "./icons/icon-96x96.png",
   "./icons/icon-128x128.png",
