18f738bf9e5cddc6cdfc798f9dd71985ac21b330
Anomalous Alpha <blackcarrera@msn.com>
2021-07-22 18:59:13 -0600

    made a connection to the indexedDB and a function to saveRecord

--- public/js/idb.js
+++ public/js/idb.js
@@ -7,8 +7,34 @@ const request = indexedDB.open('budget_tracker', 1);
 request.onupgradeneeded = function(event) {
     // save a reference to the database
     const db = event.target.result;
-    //create an obejct store (teable) called `new_wallet`, set it to have an auto incrementing primay key of sorts
+    //create an obejct store (table) called `new_wallet`, set it to have an auto incrementing primay key of sorts
     db.createObjectStore('new_wallet', {autoIncrement: true });
+}
 
-    
+// upon a successful
+request.onsuccess = function(event) {
+    // when the db is successfully created with its obect store (from onupgradeneeded event above) or simply established a connection, then save reference to the db in global variable
+    db = event.target.result;
+
+    // check if ap is online, if yes run uploadWallet() function to send all local db data to api
+    if (navigator.onLine) {
+        // uploadWallet();
+    }
+};
+
+request.onerror = function(event) {
+    // log error here
+    console.log(event.target.errorCode);
+}
+
+// this function will be executed if attempt to submit a new wallet and there's no interenet connection
+function saveRecord(record) {
+    // open a new transaction with the database with read and write permissions
+    const transaction = db.transaction(['new_Wallet'], 'readwrite');
+
+    // access the object store for `new_Wallet`
+    const walletObjectStore = transaction.createObjectStore('new_wallet');
+
+    // add record ot your store with add method
+    walletObjectStore.add(record);
 }
